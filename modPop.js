c632d37b542225b8d53c9202b0401b051424a64e
Paweł Kołodziejczak <modwebpro@gmail.com>
2018-06-17 14:49:35 +0200

    remove Modweb framework dependency

--- modPop.js
+++ modPop.js
@@ -1,5 +1,3 @@
-// create clean modal popup without any dependency
-
 export let modalPop = function ({id = 'mPop', msg = 'Success', width = '700px', background = 'transparent', border = '10px', shadow = '4px 6px 20px rgba(0,0,0,.3)', zIndex = '900', btnMsg = 'OK', btnClass = 'btn btn--ok', cb, cbInit = 'click'}) {
   this.init(id, msg, width, background, border, shadow, zIndex, btnMsg, btnClass, cb, cbInit);
 };
